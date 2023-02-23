abd18293936bf3b6f50cb9e4d4bd0bfbdde4e62b
Mayank Sinha <mayanksinha222000@gmail.com>
2023-02-24 00:11:11 +0530

    Add files via upload

---
+++ practice_08.java
@@ -0,0 +1,13 @@
+package com.internshala;
+class Cake {
+    String cakeName, flavour;
+    int price;
+    boolean isAvailable;
+}
+    public class practice_08 {
+        public static void main(String[] args) {
+            Cake ck = new Cake();
+            ck.flavour = "Black Forest";
+            System.out.println(ck.flavour);
+        }
+    }
